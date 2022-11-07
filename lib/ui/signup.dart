356ceb1cda32c3a10697eed9f875be701420d1d9
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2022-11-07 21:37:45 +0000

    added signuppage widget along with daylee app

--- lib/ui/signup.dart
+++ lib/ui/signup.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+class SignUpPage extends StafefulWidget {
+  final String title;
+  const SignUpPage({super.key, required this.title});
+
+  @override
+  State<SignUpPage> createState() => _SignUppage()
+}
+
+
+Class _SignUpPage extends State<SignUpPage>{
+
+@override
+Widget build(BuildContext context){
+  return Scaffold()
+}
+}
